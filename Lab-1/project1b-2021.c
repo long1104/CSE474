07e5a4105d16457bc7a4fe21c4b66ff955b6f3a0
longnguyen <long1104@uw.edu>
2021-01-17 16:36:48 -0800

    trouble shooting

--- Lab-1/project1b-2021.c
+++ Lab-1/project1b-2021.c
@@ -2,13 +2,50 @@
 
 //  this is a simple routine that demonstrates how to fill and diaplay an array of characters
 
+/* Old version:
+        int i = 0;                                                                      //  declare a working variable
+
+        char myArray[5];                                                        //  declare a character array
+
+        for (i = 0; i <= 5; i++)                                        //  fill array with characters
+        {
+                //  fill with the ascii characters A..F
+                //  65 is the ascii value for A
+
+                myArray[i]= 65+i;
+        }
+
+        for (i = 0; i <= 5; i++)                                        //  display the array
+        {
+                printf("%c \n", myArray[i]);
+        }
+
+        printf("\n");
+
+        return;
+*/
+
+// The error from the old version is that the array of characters is declared as length of 5.
+// However, we want to print 6 characters for A to F. So when we start to fill the arrays with
+// 6 characters (A to F), it would be not yelling any errors since C doesn't have out of bound
+// checking, which it will allow you write to the index that out of bound. However, the out of
+// bound index that you write will not guarantee to hold the value you put in since it is not
+// registerd at the declaration of the array. Thus, later it might be used to stored different value
+// by computer. That is the reason why when you try to print the value at index 5 of arrays 
+// (which is out of bound), it will gives you some weird character that is different from the one you stored
+// and you don't expect it. That is the bug in the snippet of codes above.
+// 
+// In order to fix it, we can just simply declare the array as length of 6, which is now the index 5
+// will be registered for holding a character and will not be used for other purpose.
+
+
 void main(void)
 {
 	int i = 0;									//  declare a working variable
 
-	char myArray[5];							//  declare a character array
+	char myArray[6];							//  declare a character array
 
-	for (i = 0; i < 5; i++)					//  fill array with characters
+	for (i = 0; i <= 5; i++)					//  fill array with characters
 	{
 		//  fill with the ascii characters A..F
 		//  65 is the ascii value for A
@@ -16,7 +53,7 @@ void main(void)
 		myArray[i]= 65+i;
 	}
 
-	for (i = 0; i < 5; i++)					//  display the array
+	for (i = 0; i <= 5; i++)					//  display the array
 	{
 		printf("%c \n", myArray[i]);
 	}
--- Lab-1/project1c-2021.c
+++ Lab-1/project1c-2021.c
@@ -12,8 +12,9 @@ void main (void)
 	int* myPtr = &myValue;		//  let myPtr point to myValue
 
 	//  get data from the user
+       
 	getData(myPtr);
-
+       
 	//  display the data as a character
 	printf("The data is: %c \n", *myPtr);
 
@@ -51,3 +52,33 @@ void getData(int* valuePtr)
 	return;
 
 }
+
+/* Old version of the snippet codes that cause error:
+        //declare a temp place to store the data
+        int tempValue;
+
+        //  let valuePtr point to it
+        valuePtr = &tempValue;
+
+        //  prompt for data
+        printf("Please enter a single digit between 0-9 \n");
+
+        //  get the data
+        *valuePtr = getchar();
+
+        //  display its value as a character
+        printf("The data is: %c \n", *valuePtr);
+
+        return;
+*/
+// The error in the snippet code above is that we assigned the valuePtr
+// to a local variable stored on the stack, i.e., tempValue. Since we don't backup the address
+// stored in valuePtr before reassign it to new address, when we exit
+// the getData function, the local variable (tempValue) will be disappeared,
+// and we will never be able to retrieve the value from that variable anymore.
+// Thus, when we back in main function, and we want to print it one more, 
+// we cannot print the value stored at the address stored in valuePtr anymore.
+//
+// In order to fix this error, we can just simple back up the address stored in
+// valuePtr into a temp variable. Then add the end before quitting the getData()
+// function, we can just simply assigned input value to the backed-up address
